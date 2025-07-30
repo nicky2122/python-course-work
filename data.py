b6262ab34d649c0dcefd245c7c1599f35ad4eb3d
nicky2122 <vemula.nikitha21@gmail.com>
2025-07-30 10:50:59 +0530

    nicky

---
+++ data.py
@@ -0,0 +1,18 @@
+a=123
+b=12.56
+c=5+2j#comple
+d="nikhitha vemula"#string
+e=['nicky','python','PFS30']#list
+h=(22.3,'nicky')#set
+set={"nike","adidas","puma"}#tuple
+dic={"name:anjali","age:27","study:msc"}#dictionary
+result=True#boolean
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(h))
+print(type(set))
+print(type(dic))
+print(type(result))
