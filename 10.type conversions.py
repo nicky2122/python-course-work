e54622ccae5ff7592ae299dcb8c9ca4396dee990
nicky2122 <vemula.nikitha21@gmail.com>
2025-08-04 19:29:49 +0530

    nicky

---
+++ 10.type conversions.py
@@ -0,0 +1,33 @@
+int_val=21
+float_val=22.2
+str_val="123"
+list_val=[1,2,3,4]
+tuple_val=(2,3,1,4)
+set_val={6,7,8,9}
+dictionary_val={"a":1, "b":2}
+bool_val=True
+print("/n type conversions:")
+print("int to float: ",float(int_val))
+print("int to string: ",str(int_val))
+print("int to boolean: ",bool(int_val))
+print("float to int: ",int(float_val))
+print("float to string: ",str(float_val))
+print("float tp boolean: ",bool(float_val))
+print("sting to int: ",int(str_val))
+print("string to float: ",float(str_val))
+print("string to boolean: ",bool(str_val))
+print("list to tuple: ",tuple(list_val))
+print("list to set: ",set(list_val))
+print("list to string: ",str(list_val))
+print("tuple to list: ",list(tuple_val))
+print("tuple to set: ",set(tuple_val))
+print("tuole to string: ",str(tuple_val))
+print("set to tuple: ",tuple(set_val))
+print("set to list: ",list(set_val))
+print("set to string: ",str(set_val))
+print("dictionary to string: ",str(dictionary_val))
+print("dictionary keys to list: ",list(dictionary_val.keys()))
+print("dictionary values to list: ",list(dictionary_val.values()))
+print("boolean to int: ",int(bool_val))
+print("boolean to float: ",float(bool_val))
+print("boolean to string: ",str(bool_val))
